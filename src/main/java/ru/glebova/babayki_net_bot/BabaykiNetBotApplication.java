ad135669933ef698a49321ec602641a972bcf775
Glebova <e.tulzakova@mail.ru>
2023-04-13 13:47:56 +0400

    Initial commit

---
+++ src/main/java/ru/glebova/babayki_net_bot/BabaykiNetBotApplication.java
@@ -0,0 +1,13 @@
+package ru.glebova.babayki_net_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabaykiNetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabaykiNetBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/glebova/babayki_net_bot/BabaykiNetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.glebova.babayki_net_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabaykiNetBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
